040bc469433d71e28df92d10fee492758c11cd7c
mikael <mikael.quadros@gmail.com>
2023-10-18 16:26:25 -0300

    add configs

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,18 @@
 import './bootstrap';
+import { Tooltip, Toast, Popover } from 'bootstrap';
+
+// Tooltips
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new Tooltip(tooltipTriggerEl))
+
+// Toasts
+const toastElList = document.querySelectorAll('.toast')
+const toastList = [...toastElList].map(toastEl => new Toast(toastEl))
+toastList.forEach(toast => toast.show());
+
+// Popover
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new Popover(popoverTriggerEl))
+
+import jQuery from 'jquery';
+window.$ = jQuery;
