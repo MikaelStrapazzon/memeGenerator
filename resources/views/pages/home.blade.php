45e326cc8617075b8f4deced2721b744c795e9d5
mikael <mikael.quadros@gmail.com>
2023-10-31 03:01:07 -0300

    Esqueleto tela Home

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,3 +1,25 @@
-<x-noLogged-layout>
-    <h1>TESTE</h1>
-</x-noLogged-layout>
+<x-default-layout>
+    <div class="container-fluid d-flex align-items-center justify-content-center">
+        <div class="row container-xl">
+
+            <div class="col-sm-6 pe-0">
+                <div class="card">
+                    <div style="background-color: dimgray; height: 600px; width: 400px">
+                        Imagem
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-6 ps-0">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Modificações</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        <a href="#" class="btn btn-primary">Gerar Meme</a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</x-default-layout>
