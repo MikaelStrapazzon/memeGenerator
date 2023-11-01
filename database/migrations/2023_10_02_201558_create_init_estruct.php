ef768ca3893e1294cbfc8446e3759ba30858cd92
mikael <mikael.quadros@gmail.com>
2023-11-01 01:18:34 -0300

    Alterado a tabela de usuários para a tabela de Users padrão

--- database/migrations/2023_10_02_201558_create_init_estruct.php
+++ database/migrations/2023_10_02_201558_create_init_estruct.php
@@ -13,30 +13,18 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('tb_usuario', function (Blueprint $table) {
-            $table->BigIncrements('id_usuario');
-            $table->String('nm_usuario');
-            $table->String('email_usuario');
-            $table->String('senha_usuario');
-            $table->boolean('admin');
-
-            $table->timestamps();
-
-            $table->index('id_usuario');
-        });
-
         Schema::create('tb_template_gerado', function (Blueprint $table) {
             $table->id('id_template_gerado');
             $table->unsignedBigInteger('id_template');
             $table->unsignedBigInteger('id_usuario');
             $table->String('nm_template_gerado');
-   
+
         });
 
         Schema::create('tb_template', function (Blueprint $table) {
             $table->BigIncrements('id_template');
             $table->String('nm_template');
-    
+
             $table->timestamps();
 
             $table->index('id_template');
@@ -49,33 +37,32 @@ return new class extends Migration
             $table->integer('x_tam');
             $table->integer('z_tam');
             $table->integer('fonte_tam');
-    
+
         });
 
         Schema::table('tb_template_gerado', function (Blueprint $table) {
-    
+
             $table->foreign('id_template')->references('id_template')->on('tb_template');
-            $table->foreign('id_usuario')->references('id_usuario')->on('tb_usuario');
+            $table->foreign('id_usuario')->references('id')->on('users');
            });
 
 
     }
-    
+
     /**
      * Reverse the migrations.
      */
     public function down(): void
-    { 
+    {
         Schema::table('tb_template_gerado', function (Blueprint $table) {
             $table->dropForeign(['id_template']);
             $table->dropForeign(['id_usuario']);
         });
-    
+
         Schema::table('tb_posicao_texto', function (Blueprint $table) {
             $table->dropForeign(['id_template']);
         });
-    
-        Schema::dropIfExists('tb_usuario');
+
         Schema::dropIfExists('tb_template_gerado');
         Schema::dropIfExists('tb_template');
         Schema::dropIfExists('tb_posicao_texto');
