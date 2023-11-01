6315a39f037ee27ebeafc5bc5442d041ae75d780
mikael <mikael.quadros@gmail.com>
2023-11-01 00:55:15 -0300

    Atualizado header padrão

--- resources/views/components/headers/header-default.blade.php
+++ resources/views/components/headers/header-default.blade.php
@@ -1,41 +1,31 @@
-<header>
-    <nav class="
-        navbar
-        bg-primary
-        shadow
-        px-4
-        d-flex flex-column flex-nowrap
-        flex-sm-row
-    ">
-        <x-logo class="wf-45" />
-        <a class="navbar-brand text-white ms-2 p-0" href="{{ route('home') }}">
-            <b>MemeG</b><span class="fs-6">enerator</span>
-        </a>
-
-        <div class="
-            navbar-nav
-            d-flex flex-row
-            justify-content-end
-            w-100
-        ">
-            <a href="{{ route('login') }}" class="text-white text-decoration-none">
-                Entrar
+<header class="p-2 mb-3 border-bottom bg-primary shadow">
+    <div class="container">
+        <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+            <a href="{{ route('home') }}" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+                <x-logo class="wf-45" />
+                <b class="ms-2 fs-5">MemeG</b><span>enerator</span>
             </a>
-{{--                <b class="text-white me-1">--}}
-{{--                    Auth::user()->name--}}
-{{--                </b>--}}
 
-{{--                <div>--}}
-{{--                    <form id="logout-form" action="{{ route('logout') }}" method="POST">--}}
-{{--                        @csrf--}}
-{{--                        <input--}}
-{{--                            type="submit"--}}
-{{--                            class="btn text-white py-0"--}}
-{{--                            value="{{ __('common.logout') }}"--}}
-{{--                        />--}}
-{{--                    </form>--}}
-{{--                </div>--}}
+            <span class="nav col-12 col-lg-auto me-lg-auto mb-2 mb-md-0">
+            </span>
+
+            <div class="dropdown text-end">
+                @if(Auth::check())
+                    <a href="#" class="d-flex text-decoration-none dropdown-toggle align-items-center text-white" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+                        <x-icon-user class="wf-16 me-2" />
+                        <b>Mario</b>
+                    </a>
+                    <ul class="dropdown-menu text-small" aria-labelledby="dropdownUser1" style="">
+                        <li><a class="dropdown-item" href="#">Histórico</a></li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li><a class="dropdown-item" href="#">Deslogar</a></li>
+                    </ul>
+                @else
+                    <a href="{{ route('login') }}">
+                        <button type="button" class="btn btn-outline-light">Entrar</button>
+                    </a>
+                @endif
+            </div>
         </div>
-
-    </nav>
+    </div>
 </header>
