71dd2ff46fcca8452a3b3f7cfed7a05e22424a9b
Lucas Cesar <93329502+Luc-cC@users.noreply.github.com>
2023-11-22 14:39:05 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TemplateController;
 
 Route::fallback(function () {
     return redirect(Route('home'));
@@ -10,6 +11,20 @@ Route::fallback(function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// ADICIONAR TEMPLATE
+Route::get('add-template', [TemplateController::class, 'create'])->name('Template.create');
+Route::post('add-template', [TemplateController::class, 'store'])->name('Template.store');
+
+// LISTA
+Route::get('/templates', [TemplateController::class, 'list'])->name('templates.list');
+
+// DELETAR
+Route::delete('/templates/{template}', [TemplateController::class, 'destroy'])->name('templates.destroy');
+
+// EDITAR
+Route::get('/templates/{template}/edit', [TemplateController::class, 'edit'])->name('templates.edit');
+Route::put('/templates/{template}', [TemplateController::class, 'update'])->name('templates.update');
 
 Route::middleware(['auth'])->group(function () {
 
